02b89dd3485aafc5b2aec3160bceeb9b96290b5b
Kauê Fernandes <90159661+fernandes-kaue@users.noreply.github.com>
2024-08-29 20:44:43 -0300

    adding serial annotations to the serializable variable

--- jpa/src/main/java/models/BookModel.java
+++ jpa/src/main/java/models/BookModel.java
@@ -3,20 +3,22 @@ package models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @Entity
-@Table(name= "TB_BOOKS")
+@Table(name= "TB_BOOK")
 public class BookModel implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @Column(nullable = false, unique = false)
+    @Column(nullable = false)
     private String title;
 
 
--- jpa/src/main/java/models/PublisherModel.java
+++ jpa/src/main/java/models/PublisherModel.java
@@ -3,6 +3,7 @@ package models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "TB_PUBLISHER")
 public class PublisherModel implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
